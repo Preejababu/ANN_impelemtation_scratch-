251df5940c5d01dc4536c5ff5172b9e847912bf0
Preeja Babu <preejaprathap@gmail.com>
2021-10-08 21:57:39 +0530

    configration changes

---
+++ setup.py
@@ -0,0 +1,22 @@
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Preejababu,
+    description="A small package for ANN Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Preejababu/ANN_impelemtation_scratch",
+    author_email="preejaprathap@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        "tensorflow",
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "pandas"
+    ]
+)
