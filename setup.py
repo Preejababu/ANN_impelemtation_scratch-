c6b0c5a9185dc2269dbc3e49f10b4fefdb664bcb
Preeja Babu <preejaprathap@gmail.com>
2021-10-09 12:22:09 +0530

    model building

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="src",
     version="0.0.1",
-    author="Preejababu,
+    author="Preejababu",
     description="A small package for ANN Implementation",
     long_description=long_description,
     long_description_content_type="text/markdown",
